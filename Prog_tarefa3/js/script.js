7b72bb37c05acf7a8f92693c0db8b446303d2b96
lucasferreira97 <lucas97.silva@catolicasc.org.br>
2017-04-17 21:48:04 -0300

    Add files via upload

--- Prog_tarefa3/js/script.js
+++ Prog_tarefa3/js/script.js
@@ -1,10 +1,6 @@
-// NOTE que este script foi o usado no exercício Ajax 4 e não vai funcionar
-// automaticamente com seu código. Você deve adaptá-lo, 
-// especialmente a função geraPaginaPrincipal, conforme
-// seu template HTML.
-
-var dataUrl = "data.json",
-    itensHtml = "item-snippet.html";
+var dataUrl = "dados/menu.json",
+    itensHtml = "js/menu.html";
+	itensHtml2 = "js/item.html";
     
 // função facilitadora para inserir HTML em um elemento
 function insereHtml(seletor, html) {
@@ -22,36 +18,49 @@ function inserePropriedade(template, propName, propValue) {
   var propriedade = "{{" + propName + "}}";
   // substitui todas as ocorrências de propriedade por propValue
   // em template
-  template = template.replace(new RegExp(propriedade, "g"), propValue);
+  template = template.replace(new RegExp(propriedade, "g"),
+              propValue);
   return template;
 }
 
 // constroi a pagina, com os dados recebidos por parametro
 function constroiPagina(dados) {
-  $ajaxUtils.sendGetRequest(itensHtml, geraPaginaPrincipal, false); // não é um JSON
-}
-
-function geraPaginaPrincipal(itensHtml) {
-    var htmlFinal = '<section class="row">'; // string que vai conter todo o HTML
-    // construimos os itens agora
+  var htmlFinal = ''; // string que vai conter todo o HTML
+  var htmlFinal2 = '';
+  // construimos os itens agora
+  $ajaxUtils.sendGetRequest(itensHtml, function(itensHtml) {
     for (var i = 0, max = dados.length; i < max; i++) {
-        var html = itensHtml,
-            nome = dados[i].name.first + " " + dados[i].name.last,
-            empresa = dados[i].company,
-            email = dados[i].email,
-            fone = dados[i].phone;
+      var html = itensHtml,
+          titulo = dados[i].titulo ;
           
-        html = inserePropriedade(html, "nome", nome);
-        html = inserePropriedade(html, "empresa", empresa);
-        html = inserePropriedade(html, "email", email);
-        html = inserePropriedade(html, "fone", fone);
-
-        htmlFinal += html;
+      html = inserePropriedade(html, "titulo", titulo);      
+      htmlFinal += html;
     }
-    htmlFinal += '</section>';
-    insereHtml("#content", htmlFinal);
+    insereHtml("ul", htmlFinal);
+  }, false); // não é um JSON
+    
+    
+      $ajaxUtils.sendGetRequest(itensHtml2, function(itensHtml2) {
+    for (var i = 0, max = dados.length; i < max; i++) {
+      var html = itensHtml2,
+          titulo = dados[i].titulo,
+          conteudo = dados[i].conteudo;
+          
+      html = inserePropriedade(html, "titulo", titulo); 
+      html = inserePropriedade(html, "conteudo", conteudo);  
+      htmlFinal2 += html;
+    }
+    insereHtml(".row", htmlFinal2);
+  }, false); // não é um JSON
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
 // vamos construir o sendGetRequest:
 // definir a URL (dataUrl)
 // e o metodo constroiPagina
